ff715e0e118b253530d126a1db97e64c1032006f
Sam Priddy <priddy.samuel@gmail.com>
2018-07-11 11:57:01 -0400

    add tables create route and error validation for new table page

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -10,4 +10,19 @@ class TablesController < ApplicationController
   def new
     @table = Table.new()
   end
+
+  def create
+    @table = Table.new(table_params)
+
+    if @table.save
+      redirect_to table_path(@table)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def table_params
+    params.require(:table).permit(:name, :seats)
+  end
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,5 @@
 class Table < ApplicationRecord
   has_many :reservations
+  validates :name, presence: true
   validates :seats, presence: true, inclusion: { in: 2..10 }
 end
