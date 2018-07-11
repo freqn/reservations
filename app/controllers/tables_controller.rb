2bcede8212c23b9b62b5d7ea439c11c68234f31f
Sam Priddy <priddy.samuel@gmail.com>
2018-07-11 11:50:50 -0400

    Add new table page

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,9 +1,13 @@
 class TablesController < ApplicationController
   def index
-    @tables = Table.all
+    @tables = Table.all()
   end
 
   def show
     @table = Table.find(params[:id])
+  end
+
+  def new
+    @table = Table.new()
   end
 end
