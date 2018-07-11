c601f9efb5269723bcab392111bc3343249f6550
Sam Priddy <priddy.samuel@gmail.com>
2018-07-11 13:22:54 -0400

    Ability to make reservations (not user friendly yet)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,19 @@
 class ReservationsController < ApplicationController
+  def index
+    @table = Table.find(params[:table_id])
+  end
+
+  def new
+    @table = Table.find(params[:table_id])
+  end
+
+  def create
+    @table = Table.find(params[:table_id])
+    @reservation = @table.reservations.create!(reservation_params)
+    redirect_to table_path(@table)
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start, :duration)
+  end
 end
