354c4da07f80239083887eae33f3973f9234e463
Sam Priddy <priddy.samuel@gmail.com>
2018-07-11 17:46:24 -0400

    add email column to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_174918) do
+ActiveRecord::Schema.define(version: 2018_07_11_214614) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "table_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_07_11_174918) do
     t.datetime "updated_at", null: false
     t.integer "party_size"
     t.datetime "end_time"
+    t.string "email"
     t.index ["table_id"], name: "index_reservations_on_table_id"
   end
 
