223b3556035b649edf03d093c60eae471d91c514
Sam Priddy <priddy.samuel@gmail.com>
2018-07-11 23:02:47 -0400

    explicitly set tls as smtp protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
+    :protocol             => "tls",
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
